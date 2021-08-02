a372236bef6ec91db375557bc2e18458104bc74a
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2021-08-02 08:03:22 -0300

    Instalando o Redux e Preparando as pastas

---
+++ src/app/reducers/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { combineReducers } from 'redux';
+
+const reducers = combineReducers({
+  root: (state, action) => state || {},
+});
+
+export default reducers;
